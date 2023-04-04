9d1766764a9b766c072cfb6f52c70969add85c2c
ly-fanxq <fanxq@szlanyou.com>
2023-04-04 23:13:11 +0800

    【基础功能】使用信号量实现按键控制LED

--- ra_gen/common_data.c
+++ ra_gen/common_data.c
@@ -3,6 +3,21 @@
 ioport_instance_ctrl_t g_ioport_ctrl;
 const ioport_instance_t g_ioport =
 { .p_api = &g_ioport_on_ioport, .p_ctrl = &g_ioport_ctrl, .p_cfg = &g_bsp_pin_cfg, };
+SemaphoreHandle_t g_sw1_semaphore;
+#if 1
+StaticSemaphore_t g_sw1_semaphore_memory;
+#endif
+void rtos_startup_err_callback(void *p_instance, void *p_data);
 void g_common_init(void)
 {
+    g_sw1_semaphore =
+#if 1
+            xSemaphoreCreateBinaryStatic (&g_sw1_semaphore_memory);
+#else
+                xSemaphoreCreateBinary();
+                #endif
+    if (NULL == g_sw1_semaphore)
+    {
+        rtos_startup_err_callback (g_sw1_semaphore, 0);
+    }
 }
--- ra_gen/common_data.h
+++ ra_gen/common_data.h
@@ -3,6 +3,8 @@
 #define COMMON_DATA_H_
 #include <stdint.h>
 #include "bsp_api.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
 #include "r_ioport.h"
 #include "bsp_pin_cfg.h"
 FSP_HEADER
@@ -11,6 +13,7 @@ extern const ioport_instance_t g_ioport;
 
 /* IOPORT control structure. */
 extern ioport_instance_ctrl_t g_ioport_ctrl;
+extern SemaphoreHandle_t g_sw1_semaphore;
 void g_common_init(void);
 FSP_FOOTER
 #endif /* COMMON_DATA_H_ */
